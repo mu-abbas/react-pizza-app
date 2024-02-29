e501757ad035099b45aa114917acb5a9f1a41c2b
Muhammad Abbas <md.devjs@gmail.com>
2024-02-29 15:53:32 +0300

    adding file structure and configure react router

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        loading: {
+          '20%': {
+            backgroundPosition: '0% 0%, 50% 50%, 100% 50%',
+          },
+          '40%': {
+            backgroundPosition: '0% 100%, 50% 0%, 100% 50%',
+          },
+          '60%': {
+            backgroundPosition: '0% 50%, 50% 100%, 100% 0%',
+          },
+          '80%': {
+            backgroundPosition: '0% 50%, 50% 50%, 100% 100%',
+          },
+        },
+      },
+      animation: {
+        loading: 'loading 1s infinite ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+};
