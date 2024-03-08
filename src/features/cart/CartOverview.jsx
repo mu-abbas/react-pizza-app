5ac2de13f99397337839bb64e277eae8d3c071c1
Muhammad Abbas <md.devjs@gmail.com>
2024-03-08 11:34:48 +0300

    fix overview issue

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -7,19 +7,19 @@ function CartOverview() {
   const totalPrice = cart.reduce((sum, item) => sum + item.totalPrice, 0).toFixed(2);
 
   return (
-    <>
-      {!!totalQuantity && (
-        <div className="flex items-center justify-between p-4 bg-stone-800 text-stone-300 md:px-8">
-          <p className="space-x-2 text-sm font-semibold uppercase md:text-base ">
-            <span>{totalQuantity} pizzas</span>
-            <span>${totalPrice}</span>
-          </p>
-          <Link to="/cart" className="tracking-tight uppercase transition duration-300 hover:text-stone-100">
-            Open cart &rarr;
-          </Link>
-        </div>
-      )}
-    </>
+    <div
+      className={`flex items-center justify-between p-4 bg-stone-800 text-stone-300 md:px-8 ${
+        totalQuantity > 0 ? `flex` : 'hidden'
+      }`}
+    >
+      <p className="space-x-2 text-sm font-semibold uppercase md:text-base ">
+        <span>{totalQuantity} pizzas</span>
+        <span>${totalPrice}</span>
+      </p>
+      <Link to="/cart" className="tracking-tight uppercase transition duration-300 hover:text-stone-100">
+        Open cart &rarr;
+      </Link>
+    </div>
   );
 }
 
